d45cb0b71c77f634fcd9771e2d6a59ac4d8c01a4
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-03-20 12:42:26 -0400

    Create psutils.ps1

---
+++ reg/psutils.ps1
@@ -0,0 +1,13 @@
+{
+  "name": "psutils",
+  "vendor": "lukesampson",
+  "version": "1.0.0",
+  "license": "MIT",
+  "description": "Give your PowerShell some pizazz.",
+  "git_url": "git@github.com:lukesampson/psutils.git",
+  "global": "true",
+  "bin": [ 
+    "gitignore.ps1",
+    "say.ps1"
+  ]
+}
